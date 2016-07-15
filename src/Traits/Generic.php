4d95fe0d6056a9cf7df5d4b3e240b3118ce3e184
Fabian Bircher <f.bircher@gmail.com>
2016-07-15 12:09:31 +0200

    Then :first should precede :second

--- src/Traits/Generic.php
+++ src/Traits/Generic.php
@@ -316,4 +316,22 @@ trait Generic {
     }
   }
 
+  /**
+   * @Then :first should precede :second
+   */
+  public function shouldPrecede($first, $second) {
+    $page = $this->getSession()->getPage()->getText();
+    $pos1 = strpos($page, $first);
+    if ($pos1 === FALSE) {
+      throw new ExpectationException("Text not found: '$first'.", $this->getSession());
+    }
+    $pos2 = strpos($page, $second);
+    if ($pos2 === FALSE) {
+      throw new ExpectationException("Text not found: '$second'.", $this->getSession());
+    }
+    if ($pos2 < $pos1) {
+      throw new \Exception("Text '$first' does not precede text '$second'.");
+    }
+  }
+
 }
