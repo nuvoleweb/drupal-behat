430dc437cac88d4c94761597735f7d810cd3a127
Antonio De Marco <antonio@nuvole.org>
2016-09-15 16:37:56 +0200

    Perform PHPCS as a pre-push hook.

--- scripts/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Setting up Git pre-commit hook."
-cp scripts/pre-commit.sh .git/hooks/pre-commit
-chmod +x .git/hooks/pre-commit
-
-echo "Setting up Code Sniffer."
-vendor/bin/phpcs --config-set installed_paths ../../drupal/coder/coder_sniffer
-vendor/bin/phpcs --config-set show_progress 1
